17e3c8248fdeccac90478be5b316b7ba7ef3dbd9
wangjinlong <wangjinlong@mockuai.com>
2020-03-09 13:30:33 +0800

    1

--- redisson/src/main/java/org/redisson/RedissonLockEntry.java
+++ redisson/src/main/java/org/redisson/RedissonLockEntry.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2018 Nikita Koksharov
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,10 +22,22 @@ import org.redisson.misc.RPromise;
 
 public class RedissonLockEntry implements PubSubEntry<RedissonLockEntry> {
 
+    /**
+     * 计数器
+     *
+     * 每次发起订阅，则计数器 + 1
+     * 每次取消订阅，则计数器 - 1 。当减少到 0 时，才正常取消订阅。
+     */
     private int counter;
 
+    /**
+     * 信号量，用于实现 RedissonLock 阻塞等待的通知
+     */
     private final Semaphore latch;
     private final RPromise<RedissonLockEntry> promise;
+    /**
+     * 监听器们
+     */
     private final ConcurrentLinkedQueue<Runnable> listeners = new ConcurrentLinkedQueue<Runnable>();
 
     public RedissonLockEntry(RPromise<RedissonLockEntry> promise) {
@@ -34,14 +46,17 @@ public class RedissonLockEntry implements PubSubEntry<RedissonLockEntry> {
         this.promise = promise;
     }
 
+    @Override
     public void aquire() {
         counter++;
     }
 
+    @Override
     public int release() {
         return --counter;
     }
 
+    @Override
     public RPromise<RedissonLockEntry> getPromise() {
         return promise;
     }
